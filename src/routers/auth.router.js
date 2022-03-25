258fc68ba6544d63e420ecf6a3046d6853917059
thedan158 <19521312@gm.uit.edu.vn>
2022-03-25 16:05:49 +0700

    feature - owner create employee account

--- src/routers/auth.router.js
+++ src/routers/auth.router.js
@@ -3,7 +3,7 @@ const router = express.Router();
 
 import {AuthController} from "../controllers/auth.controller.js";
 
-router.post('/createUser', AuthController.createUser);
+router.post('/createUser/:role', AuthController.createUser);
 router.get('/login', AuthController.loginUser);
 router.post('/register', AuthController.registerUser);
 
