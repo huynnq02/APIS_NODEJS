bc5b55615dd598202d3bd2264826091a8deebd4d
huyy802 <20521401@gm.uit.edu.vn>
2022-04-23 08:29:22 +0700

    feature-food

--- src/routers/food.router.js
+++ src/routers/food.router.js
@@ -3,7 +3,7 @@ const router = express.Router();
 
 import { FoodController } from "../controllers/food.controller.js";
 
-router.post("/addFood/:menuID", FoodController.addFood);
+router.post("/addFood/", FoodController.addFood);
 router.delete("/deleteFood/:id", FoodController.deleteFood);
 router.put("/updateFood/:id", FoodController.updateFood);
 
