54321cb17fb7ef15f635851230e8081b0b41d7af
thedan158 <19521312@gm.uit.edu.vn>
2022-05-25 04:44:14 +0700

    fix

--- src/index.js
+++ src/index.js
@@ -74,7 +74,7 @@ app.use("/orderInfo", orderInfoRouters);
 //   });
 
 const PORT = process.env.PORT || 3000;
-server.listen(PORT,'0.0.0.0', () => {
+server.listen(PORT, () => {
   console.log(`Our app is running on port ${ PORT }`);
 });
 //#end region
