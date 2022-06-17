0e55fbfbe11aee4cb93be1f807f5b41f7985acb1
huyy802 <20521401@gm.uit.edu.vn>
2022-06-17 22:13:34 +0700

    feature-updateTableCondition

--- src/routers/table.router.js
+++ src/routers/table.router.js
@@ -6,6 +6,8 @@ import { TableController } from "../controllers/table.controller.js";
 router.post("/createTable", TableController.createTable);
 router.delete("/deleteTable/:id", TableController.deleteTable);
 router.put("/updateTable/:id", TableController.updateTable);
+router.put("/updateBusyTable", TableController.updateBusyTable);
+
 router.get("/getAllTable/:restaurantID", TableController.getAllTable);
 router.post(
   "/getAllTableOfRestaurant",
