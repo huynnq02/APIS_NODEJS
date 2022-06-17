9f65c94c2ce25f62d10bed2441ea22c77f4a7654
thedan158 <19521312@gm.uit.edu.vn>
2022-06-17 22:13:17 +0700

    update - auth router

--- src/routers/auth.router.js
+++ src/routers/auth.router.js
@@ -6,7 +6,7 @@ import { AuthController } from "../controllers/auth.controller.js";
 router.post("/createUser/:usernameOwner", AuthController.createUser);
 router.post("/login", AuthController.loginUser);
 router.post("/register", AuthController.registerUser);
-router.post("/getAllUser", AuthController.getAllUser);
+router.get("/getAllUser/:username", AuthController.getAllUser);
 router.post("/hasNoRestaurant", AuthController.hasNoRestaurant);
 router.put("/updateUser", AuthController.updateUser);
 router.put("/changePassword", AuthController.changePassword);
