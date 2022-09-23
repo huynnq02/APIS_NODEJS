10a5cb7f39c8bc82e2489f6877da2cf1ad560afd
huyy802 <20521401@gm.uit.edu.vn>
2022-09-23 23:43:42 +0700

    Fix middlewares

--- src/routers/restaurant.router.js
+++ src/routers/restaurant.router.js
@@ -1,11 +1,19 @@
 import express from "express";
 const router = express.Router();
+import verifyToken from "../middlewares/auth.js";
 
 import { RestaurantController } from "../controllers/restaurant.controller.js";
 
-router.post("/createRestaurant", RestaurantController.createRestaurant);
+router.post(
+  "/createRestaurant",
+  verifyToken,
+  RestaurantController.createRestaurant
+);
 router.delete("/deleteRestaurant/:id", RestaurantController.deleteRestaurant);
-router.post("/updateRestaurant/:username", RestaurantController.updateRestaurant);
+router.post(
+  "/updateRestaurant/:username",
+  RestaurantController.updateRestaurant
+);
 router.post(
   "/checkRestaurantNotExists/",
   RestaurantController.checkRestaurantNotExists
